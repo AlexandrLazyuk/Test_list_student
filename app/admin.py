f3b00266dafcee4b0c6daf87741c89688c8f0710
Alexandr Lazyuk <Zuk8080@mail.ru>
2021-09-19 20:03:28 +0300

    Added command

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from app.models import Subject, UserGroup, UniversityUser
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+@admin.register(UserGroup)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+@admin.register(UniversityUser)
+class UniversityUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username',)
+
+
--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class UserGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class UniversityUser(AbstractUser):
+    role_type = models.CharField(max_length=40, choices=[
+        ('teacher', 'Teacher'),
+        ('student', 'Student')
+    ])
+    subject = models.ForeignKey(Subject, related_name='title', on_delete=models.CASCADE, default=None, null=True, blank=True)
+    group = models.ManyToManyField(UserGroup, default=None, blank=True)
+
+    def __str__(self):
+        return self.username
